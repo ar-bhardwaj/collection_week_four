e69137196909e987e2b2f461bf06e01fb4a91a20
aditi <aditiranttanbhardwaj@gmail.com>
2025-03-26 14:20:27 +0530

    Implemented division program handling ArithmeticException and InputMismatchException

--- exception_handling/unchecked_exception/DivisionEx.java
+++ exception_handling/unchecked_exception/DivisionEx.java
@@ -1,31 +1,29 @@
 package exception_handling.unchecked_exception;
+
 import java.util.*;
-import java.io.*;
+
 public class DivisionEx {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        try{
-             // Taking user input
-             System.out.print("Enter numerator: ");
-             int num1 = sc.nextInt();
-             System.out.print("Enter denominator: ");
-             int num2 = sc.nextInt();
- 
-             // Performing division
-             int result = num1 / num2;
-             System.out.println("Result: " + result);
-        }
-        catch (ArithmeticException e){  // Handles division by zero
+        Scanner sc = new Scanner(System.in);
+        try {
+            // Taking user input
+            System.out.print("Enter numerator: ");
+            int num1 = sc.nextInt();
+            System.out.print("Enter denominator: ");
+            int num2 = sc.nextInt();
+
+            // Performing division
+            int result = num1 / num2;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) { // Handles division by zero
             System.out.println("Cannot be divided by zero");
-        }
-        catch(InputMismatchException e){  // Handles non-numeric input
+        } catch (InputMismatchException e) { // Handles non-numeric input
             System.out.println("Invalid Input, please enter Numbers only");
 
-        }
-        finally{
+        } finally {
             sc.close(); // Ensuring resource closure
         }
-        
+
     }
-    
+
 }
