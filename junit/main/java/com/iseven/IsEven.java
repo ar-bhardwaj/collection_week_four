80e1710e7dd0a47192562753f5694f61f5eed04c
aditi <aditiranttanbhardwaj@gmail.com>
2025-04-03 20:35:19 +0530

    [ADITI] ADD: testing for even number

---
+++ junit/main/java/com/iseven/IsEven.java
@@ -0,0 +1,10 @@
+package com.iseven;
+
+// IsEven.java
+public class IsEven {
+    // Method to check if a number is even
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
+
