ff0a1a2c2d93614114aa3b3426a0fe48690cd53c
aditi <aditiranttanbhardwaj@gmail.com>
2025-04-03 20:36:40 +0530

    [ADITI] ADD: performance testing using @ timeout

---
+++ junit/main/java/com/performancetask/PerformanceTask.java
@@ -0,0 +1,9 @@
+package com.performancetask;
+
+public class PerformanceTask {
+    // Method that simulates a long-running task
+    public void longRunningTask() throws InterruptedException {
+        Thread.sleep(3000); // Simulates a 3-second delay
+    }
+}
+
